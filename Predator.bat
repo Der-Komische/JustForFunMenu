29db26fd84de5ee119c67f084d275b8c4ea03e59
Der-Komische <73497280+Der-Komische@users.noreply.github.com>
2022-10-14 14:14:53 +0200

    Delete Predator.bat

--- Predator.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Spiele Predator Hunting Grounds
-start PHG.url
-echo ----------------------------------
-echo Predator Hunting Grounds startet...
-echo -----------------------------------
-@pause
-exit
