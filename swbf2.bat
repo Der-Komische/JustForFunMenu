6e3254cab3a236442daa8992eadb9e5d2227ec86
Der-Komische <73497280+Der-Komische@users.noreply.github.com>
2022-10-14 14:14:45 +0200

    Delete swbf2.bat

--- swbf2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Spiele Starwars Battlefront 2
-start starwarsbf2
-echo ----------------------------------
-echo Starwars Battlefront 2 startet...
-echo -----------------------------------
-@pause
-exit
